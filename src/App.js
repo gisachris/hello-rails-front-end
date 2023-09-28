59f22be178023a2638deea987a344a2a01c61aeb
gisa chris <gisachris24@gmail.com>
2023-09-28 04:52:33 -0200

    Make required changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Message from './components/message';
 import Home from './components/home';
+import Navigation from './components/navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/greeting" element={<Message />} />
