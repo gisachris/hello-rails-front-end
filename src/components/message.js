b85b1d63088bcb53442b5f6031ee55d37353bfab
gisa chris <gisachris24@gmail.com>
2023-09-28 05:07:52 -0200

    Move Fetching Action To Slice

--- src/components/message.js
+++ src/components/message.js
@@ -1,19 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectGreeting, setGreeting } from '../redux/features/messages/messagesSlice';
+import { selectGreeting, fetchGreetingAsync } from '../redux/features/messages/messagesSlice';
 
 const Message = () => {
   const dispatch = useDispatch();
   const greeting = useSelector(selectGreeting);
 
   useEffect(() => {
-    const fetchGreeting = async () => {
-      const response = await fetch('http://localhost:3000/api/v1/messages');
-      const data = await response.json();
-      dispatch(setGreeting(data.greetings));
-    };
-
-    fetchGreeting();
+    dispatch(fetchGreetingAsync());
   }, [dispatch]);
 
   return (
--- src/redux/features/messages/messagesSlice.js
+++ src/redux/features/messages/messagesSlice.js
@@ -1,20 +1,30 @@
-// slices/messageSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   greeting: '',
 };
 
+export const fetchGreetingAsync = createAsyncThunk(
+  'message/fetchGreetingAsync',
+  async () => {
+    const response = await fetch('http://localhost:3000/api/v1/messages');
+    const data = await response.json();
+    return data.greetings;
+  },
+);
+
 const messageSlice = createSlice({
   name: 'message',
   initialState,
-  reducers: {
-    setGreeting: (state, action) => {
-      state.greeting = action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreetingAsync.fulfilled, (state, action) => {
+        state.greeting = action.payload;
+      });
   },
 });
 
-export const { setGreeting } = messageSlice.actions;
 export const selectGreeting = (state) => state.message.greeting;
+
 export default messageSlice.reducer;
